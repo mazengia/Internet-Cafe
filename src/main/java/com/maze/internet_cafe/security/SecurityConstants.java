90b3759c93db2d2a1b85a385d24f277b41b25443
mazengia <mz.tesfa@gmail.com>
2025-12-10 15:40:45 +0300

    fix pom

--- src/main/java/com/maze/internet_cafe/security/SecurityConstants.java
+++ src/main/java/com/maze/internet_cafe/security/SecurityConstants.java
@@ -8,8 +8,6 @@ public class SecurityConstants {
     public static final String TOKEN_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String TOKEN_TYPE = "JWT";
-    public static final String TOKEN_ISSUER = "secure-api";
-    public static final String TOKEN_AUDIENCE = "secure-app";
 
     private SecurityConstants() {
         throw new IllegalStateException("Cannot create instance of static util class");
